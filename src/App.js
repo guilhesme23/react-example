df6cfb7c7937e8e08cc37f58e6d08f95ba83f4be
Guilherme Marques <marquesguilherme3@gmail.com>
2018-08-11 13:36:08 -0300

    Creating TextSection component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import ImageSection from './Components/image-section';
+import TextSection from './Components/text-section';
 
 class App extends Component {
   render() {
-    return (
-      <ImageSection source='img/city3.jpg' content='React Example' height='100vh'/>
-    );
+    return <div>
+        <ImageSection source="img/city3.jpg" content="React Example" height="100vh" />
+        <TextSection title="First Section" theme="dark">
+          Donec cursus euismod nulla, eu pellentesque nibh faucibus id. Nunc
+          a congue massa. Pellentesque faucibus erat augue, nec maximus diam
+          vestibulum vel. Etiam et velit euismod, rutrum ex in, venenatis
+          ex. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+          Nam eu sapien eget nulla consequat rhoncus vitae eu neque. Integer
+          aliquet tellus quis pulvinar molestie. Donec finibus dolor semper
+          elit euismod, ac egestas enim varius. Phasellus venenatis egestas
+          sapien, blandit porttitor neque efficitur eu. Mauris faucibus
+          tortor in orci egestas imperdiet. Proin bibendum tortor quis
+          imperdiet molestie.
+        </TextSection>
+      </div>;
   }
 }
 
--- src/Components/image-section.js
+++ src/Components/image-section.js
@@ -2,25 +2,25 @@ import React, { Component } from 'react'
 import './image-section.css'
 
 const styles = {
-    backgroundImage: '',
-    minHeight: '',
+	backgroundImage: '',
+	minHeight: '',
 }
 
 export default class ImageSection extends Component {
   constructor(props) {
-      super(props);
-      styles.backgroundImage = `url(${require('../' + this.props.source)})`;
-      styles.minHeight = this.props.height;
+		super(props);
+		styles.backgroundImage = `url(${require('../' + this.props.source)})`;
+		styles.minHeight = this.props.height;
   }
   render() {
     return (
-        <div class="pimg1" style={styles}>
-            <div class="ptext">
-                <span class="border">
-                    {this.props.content}
-                </span>
-            </div>
-        </div>
+			<div class="pimg1" style={styles}>
+				<div class="ptext">
+						<span class="border">
+							{this.props.content}
+						</span>
+				</div>
+			</div>
     )
   }
 }
